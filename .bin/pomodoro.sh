e59895d3f7a4b0b11dcfdcdd41d9a00e32db1fbe
aedigo <aedigo12@hotmail.com>
2021-05-03 22:00:46 -0300

    New Changes!

--- .bin/pomodoro.sh
+++ .bin/pomodoro.sh
@@ -2,8 +2,8 @@
 
 if [[ $1 = 'start' ]]
 then
-  cp -f ~/.bin/python/start.py ~/.bin/python/pomoCancel.py
-  python ~/.bin/python/pomodoro.py
+  cp -f ~/.bin/python/start.py ~/.bin/python/pomoCancel.py 
+  python ~/.bin/python/pomodoro.py &
 elif [[ $1 = 'cancel' ]]
 then
   cp -f ~/.bin/python/cancel.py ~/.bin/python/pomoCancel.py
